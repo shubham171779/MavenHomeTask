ad7680f80ff1a160e2f55edf087b8f9bd58589bf
shubham171779 <shubham171779.cse@chitkara.edu.in>
2020-02-03 23:29:06 +0530

    final commit

---
+++ src/main/java/com/epam/Sweet.java
@@ -0,0 +1,13 @@
+package com.epam;
+
+public class Sweet {
+    String name;
+    int qty;
+    double weight;
+    public Sweet(String name,int qty)
+    {
+        this.name = name;
+        this.qty = qty;
+
+    }
+}
